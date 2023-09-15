1bea5a46e46ca350831ee1f478bdef32d56c9964
Dockal <jamesdockal@gmail.com>
2023-09-15 09:24:02 -0300

    feat: create toast service

---
+++ src/services/Toast.service.ts
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify";
+
+export class ToastService {
+  error(message: string) {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  }
+
+  success(message: string) {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  }
+}
